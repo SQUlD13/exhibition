54abc44753c5f5dc0403ac65dd92271fef4330c0
SQUID13 <SQUADINIO13@gmail.com>
2021-02-01 16:09:25 +0200

    formatted html to be rendered dynamically

--- ca-gallery-master/js/main-controller.js
+++ ca-gallery-master/js/main-controller.js
@@ -1,6 +1,5 @@
 console.log('Starting up');
-
-renderPortfolio()
+$(renderPortfolio())
 
 function renderPortfolio() {
   var projects = getProjects();
@@ -8,8 +7,8 @@ function renderPortfolio() {
   var strHTML = ''
   projects.forEach(project => {
     strHTML += /*html*/ `
-            <div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+        <div class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal" onclick="renderModal('${project.id}')">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
@@ -25,4 +24,29 @@ function renderPortfolio() {
   });
   //strHTML += '</div>'
   $('#portfolio .container .row:nth-child(2)').html(strHTML)
+}
+
+function renderModal(projectId) {
+  var $container = $('.modal .modal-dialog .modal-content .container')
+  var project = getProjectById(projectId)
+  var date = new Date(project.publishedAt)
+  console.log(date)
+  var strHTML = /*html*/ `
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="modal-body">
+                <h2>${project.name}</h2>
+                <p class="item-intro text-muted">${project.title}</p>
+                <img class="img-fluid d-block mx-auto" src="img/portfolio/${project.id}.png" alt="">
+                <p>${project.desc}</p>
+                <ul class="list-inline">
+                  <li>${new Date(project.publishedAt).toLocaleDateString()}</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                  <i class="fa fa-times"></i>
+                  Close Project</button>
+              </div>
+            </div>
+          </div>`
+  $container.html(strHTML)
 }
--- ca-gallery-master/js/proj-service.js
+++ ca-gallery-master/js/proj-service.js
@@ -8,7 +8,7 @@ var gProjects = [
         desc: 'Practicing js and html basic structuring - also beginning a small dive into CSS!',
         url: 'projs/touchNums',
         labels: ["Matrixes", "mouse events", "CSS"],
-        publishedAt: 1610882040
+        publishedAt: 1610892271000
     },
     {
         id: 'minesweeper',
@@ -17,7 +17,7 @@ var gProjects = [
         desc: 'Practicing recursion and keeping a clean work flow. some basic css experimentation. makes me wanna go back and make it even more satisfying!',
         url: 'projs/minesweeper',
         labels: ["Matrixes", "mouse events", "Recursion"],
-        publishedAt: 1611400440
+        publishedAt: 1611410671000
     },
     {
         id: 'loginForm',
@@ -26,7 +26,7 @@ var gProjects = [
         desc: 'Practicing CSS to make things look satisfying, along with creating a database (Admittedly on the local cache), and manipulating it using a correct data structure.',
         url: 'projs/minesweeper',
         labels: ["CSS", "caching", "responsiveness"],
-        publishedAt: 1611400440
+        publishedAt: 1611756271000
     }
 
 ]
@@ -34,4 +34,10 @@ var gProjects = [
 
 function getProjects() {
     return gProjects;
+}
+
+function getProjectById(projectId) {
+    return gProjects.find(function (project) {
+        return project.id === projectId
+    })
 }
