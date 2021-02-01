ead3551c087b748bd9abe2a062bdf1850c047b8c
SQUID13 <SQUADINIO13@gmail.com>
2021-02-01 13:18:27 +0200

    basic proj data structure implemented

---
+++ ca-gallery-master/js/proj-service.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var gProjects = [
+    {
+        id: 'touchNums',
+        name: 'Touch Nums',
+        desc: 'Practicing js and html basic structuring - also beginning a small dive into CSS!',
+        url: 'projs/touchNums',
+        labels: ["Matrixes", "mouse events", "CSS"],
+        publishedAt: 1610882040
+    },
+    {
+        id: 'minesweeper',
+        name: 'Minesweeper',
+        desc: 'Practicing recursion and keeping a clean work flow. some basic css experimentation. makes me wanna go back and make it even more satisfying!',
+        url: 'projs/minesweeper',
+        labels: ["Matrixes", "mouse events", "Recursion"],
+        publishedAt: 1611400440
+    }
+
+]
