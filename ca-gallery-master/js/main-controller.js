de0c43c4cabf83e6cfb059d9a9edcefe290c05cf
SQUID13 <SQUADINIO13@gmail.com>
2021-02-01 14:10:12 +0200

    formatting and imaging for portfolio

--- ca-gallery-master/js/main-controller.js
+++ ca-gallery-master/js/main-controller.js
@@ -1,7 +1,13 @@
 console.log('Starting up');
 
-function renderPortfolioItem() {
-    var strHTML = /*html*/ `
+renderPortfolio()
+
+function renderPortfolio() {
+  var projects = getProjects();
+  //var strHTML = '<div class="row">'
+  var strHTML = ''
+  projects.forEach(project => {
+    strHTML += /*html*/ `
             <div class="col-md-4 col-sm-6 portfolio-item">
           <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
             <div class="portfolio-hover">
@@ -9,11 +15,14 @@ function renderPortfolioItem() {
                 <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
-            <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
+            <img class="img-fluid" src="img/portfolio/${project.id}.png" alt="">
           </a>
           <div class="portfolio-caption">
-            <h4>Threads</h4>
-            <p class="text-muted">Illustration</p>
+            <h4>${project.name}</h4>
+            <p class="text-muted">${project.title}</p>
           </div>
         </div>`
+  });
+  //strHTML += '</div>'
+  $('#portfolio .container .row:nth-child(2)').html(strHTML)
 }
--- ca-gallery-master/js/proj-service.js
+++ ca-gallery-master/js/proj-service.js
@@ -4,6 +4,7 @@ var gProjects = [
     {
         id: 'touchNums',
         name: 'Touch Nums',
+        title: 'Willing to break your own record?',
         desc: 'Practicing js and html basic structuring - also beginning a small dive into CSS!',
         url: 'projs/touchNums',
         labels: ["Matrixes", "mouse events", "CSS"],
@@ -12,10 +13,25 @@ var gProjects = [
     {
         id: 'minesweeper',
         name: 'Minesweeper',
+        title: 'A Classic revived',
         desc: 'Practicing recursion and keeping a clean work flow. some basic css experimentation. makes me wanna go back and make it even more satisfying!',
         url: 'projs/minesweeper',
         labels: ["Matrixes", "mouse events", "Recursion"],
         publishedAt: 1611400440
+    },
+    {
+        id: 'loginForm',
+        name: 'Login Form',
+        title: 'Done endless times before, surely',
+        desc: 'Practicing CSS to make things look satisfying, along with creating a database (Admittedly on the local cache), and manipulating it using a correct data structure.',
+        url: 'projs/minesweeper',
+        labels: ["CSS", "caching", "responsiveness"],
+        publishedAt: 1611400440
     }
 
 ]
+
+
+function getProjects() {
+    return gProjects;
+}
