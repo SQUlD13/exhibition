ed23a8b5ba3bc8149ddfe4bbe6c125adfb00d887
SQUID13 <SQUADINIO13@gmail.com>
2021-02-02 10:29:25 +0200

    more stylistic changes, implemented functions with jQuery instead of onclick for practice on the projects

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,15 +1,30 @@
 console.log('Starting up');
 $(renderPortfolio(),
-  $('form.mail-form').on('submit', function (event) {
-    sendEmail(event); console.log(event)
-  }))
+  assignMailingFormClick(),
+  assignProjectClicks()
+)
 
 function sendEmail(ev) {
   ev.preventDefault()
   var subject = $('#inputSubject1').val()
-  var body = $('#inputTextarea1').val()
+  var body = $('#inputTextarea1').val().replace(/\n/g, `%0d%0a`)
   var url = `https://mail.google.com/mail?view=cm&fs=1&to=squadinio13@gmail.com&su=${subject}&body=${body}`
   window.open(url)
+}
+
+function assignProjectClicks() {
+  getProjects().forEach(project => {
+    $(`#${project.id}`).click({ projectId: project.id }, function (event) {
+      renderModal(event)
+      console.log('project.id passed is', project.id)
+    })
+  })
+}
+
+function assignMailingFormClick() {
+  $('form.mail-form').on('submit', function (event) {
+    sendEmail(event); console.log(event)
+  })
 }
 
 function renderPortfolio() {
@@ -19,7 +34,7 @@ function renderPortfolio() {
   projects.forEach(project => {
     strHTML += /*html*/ `
         <div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal" onclick="renderModal('${project.id}')">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal" id="${project.id}">
             <div class="portfolio-hover rounded">
               <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
@@ -37,11 +52,11 @@ function renderPortfolio() {
   $('#portfolio .container .row:nth-child(2)').html(strHTML)
 }
 
-function renderModal(projectId) {
+function renderModal(event) {
   var $container = $('.modal .modal-dialog .modal-content .container')
-  var project = getProjectById(projectId)
+  var project = getProjectById(event.data.projectId)
   var date = new Date(project.publishedAt)
-  console.log(date)
+  console.log('rendering modal', date)
   var strHTML = /*html*/ `
           <div class="row">
             <div class="col-lg-8 mx-auto">
@@ -53,7 +68,7 @@ function renderModal(projectId) {
                 <ul class="list-inline text-muted">
                   <li>${new Date(project.publishedAt).toLocaleDateString()}</li>
                 </ul>
-                <a href="projects/${project.id}/index.html">
+                <a href="projects/${project.id}/index.html" id="game-${project.id}">
                   <button class="btn btn-success" onclick="window.open('projects/${project.id}/index.html') ">
                   <i class="fa fa-envelope-open"></i> Play Me!</button>
                 </a>
